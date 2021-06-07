285e57d9e2fbd78464b41063052eb1f80a0dbbe0
Juventino Aguilar Correa <superjuve@outlook.es>
2021-06-06 23:14:31 -0500

    Header en pagina de inicio(no funcional) 	-Se agrego el header, con el menu hamburguesa sin funcionar 	-Se agregaron los estilos del header

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.listen(3000,function(){
-    console.log("Servidor listo");
+    console.log("Servidor montado en el puerto 3000");
 });
 
 const path = require("path");
