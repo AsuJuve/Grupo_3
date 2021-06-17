79f0fb675a112071c5d5b89be6a4d9a579656a80
daviders98 <davider98@hotmail.com>
2021-06-16 22:27:01 -0600

    Se agrego detalle de producto

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.listen(3000,function(){
     console.log("Servidor listo");
 });
 
-const path = require("path");
-
 app.use(express.static(path.resolve(__dirname,"./public")));
 
 app.get("/",(req,res) => {
     res.sendFile(path.resolve(__dirname,"./views/home.html"));
+});
+
+app.get("/productDetail",(req,res) => {
+    res.sendFile(path.resolve(__dirname,"./views/productDetail.html"));
+});
+
+app.get("/productCart",(req,res) => {
+    res.sendFile(path.resolve(__dirname,"./views/productCart.html"));
 });
