862553263f5ec0b691bc5e4fcf47e4b7151f9662
Grecia Carolina Coronado <coronadogrecia.15@gmail.com>
2021-06-16 23:44:19 -0500

    pag carrito,login,registro

--- src/app.js
+++ src/app.js
@@ -9,6 +9,17 @@ const path = require("path");
 
 app.use(express.static(path.resolve(__dirname,"./public")));
 
-app.get("/",(req,res) => {
-    res.sendFile(path.resolve(__dirname,"./views/home.html"));
+app.post('/home', (req, res) => {
+    console.log(req.body)
+    res.sendFile('./home.html', { root: 'views' })
+});
+
+app.get("/Carrito",(req,res) => {
+    res.sendFile(path.resolve(__dirname,"./views/productCart.html"));
+});
+app.get("/Registro",(req,res) => {
+    res.sendFile(path.resolve(__dirname,"./views/register.html"));
+});
+app.get("/login",(req,res) => {
+    res.sendFile(path.resolve(__dirname,"./views/login.html"));
 });
