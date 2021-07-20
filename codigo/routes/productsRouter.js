4c3cd3d3dea4f67f8223e8c24d92f0754924f94c
Grecia Carolina Coronado <coronadogrecia.15@gmail.com>
2021-07-20 02:34:48 -0500

    Sprint4 terminado

--- codigo/routes/productsRouter.js
+++ codigo/routes/productsRouter.js
@@ -10,9 +10,9 @@ router.post('/create', productsController.store);
 
 router.get("/:id", productsController.showDetail)
 router.put("/:id", productsController.edited)
-/* */
+
 router.post("/:id", productsController.edited)
-/* */
+
 router.delete("/:id", productsController.delete)
 
 router.get("/:id/editCourse", productsController.edit)
