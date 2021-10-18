8416bb93064b62b420c66f9f62f06bf945e0c330
daviders98 <davider98@hotmail.com>
2021-10-18 17:50:21 -0600

    added cart

--- codigo/controllers/mainController.js
+++ codigo/controllers/mainController.js
@@ -70,11 +70,10 @@ module.exports={
     addToCart: async function(req,res){
         const id= parseInt(req.params.id);
         const product = await db.Product.findByPk(parseInt(id),{raw:true});
-        let userLogged = null
         if(req.session.userLogged){
             const customer = await db.Customer.findOne({raw:true,where:{
                 customer_email:req.session.userLogged
-            }});
+            }})
             const cart = await db.Shopping_cart.findOne({
                 raw:true,
                 where:{
@@ -82,8 +81,10 @@ module.exports={
                 }
             })
             const checkProductInCart = await db.Shopping_cart_products.findOne({raw:true,where:{
-                product_id:product.product_id
+                product_id:product.product_id,
+                shopping_cart_id:cart.shopping_cart_id
             }})
+            
             if(checkProductInCart==null){
                 const finalAmount = cart.amount += product.product_price;
                 const cartProduct = await db.Shopping_cart_products.create({
@@ -104,11 +105,30 @@ module.exports={
     },
     deleteFromCart: async function(req,res){
         const data = req.body;
-        console.log(data);
-        const deletedCourse = await db.Shopping_cart_products.destroy({where:{
-            product_id:data.courseId
-        }});
-        const cart = await db.Shopping_cart.update()
+        let quantity;
+        const customer = await db.Customer.findOne({raw:true,where:{
+            customer_email:req.session.userLogged
+        }})
+        const oldCart = await db.Shopping_cart.findOne({
+            raw:true,
+            where:{
+                customer_id:customer.customer_id
+            }
+        })
+        data.courseId = parseInt(data.courseId);
+        const deletedCourse = await db.Shopping_cart_products.destroy({
+            where: {
+               product_id: data.courseId,
+               shopping_cart_id:oldCart.shopping_cart_id
+            }
+         });
+        quantity = oldCart.amount += deletedCourse.product_price;
+        const cart = await db.Shopping_cart.update({
+            customer_id:customer.customer_id,
+            amount:quantity
+        },{where:{
+            customer_id:customer.customer_id
+        }})
         res.redirect("/productCart");
     }
 }
